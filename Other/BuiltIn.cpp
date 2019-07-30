8466a3b34e90c7eca9b09a2d0ba8a224ecfc4a78
Kelly Lu <37441523+Kelly2336@users.noreply.github.com>
2019-07-30 09:57:08 +0800

    Create BuiltIn.cpp

---
+++ Other/BuiltIn.cpp
@@ -0,0 +1,23 @@
+//gcc專用
+//unsigned int ffs
+//unsigned long ffsl
+//unsigned long long ffsll
+#include<stdio.h>
+int main()
+{
+    unsigned int x;
+    while(scanf("%u",&x)==1)
+    {
+        printf("右起第一個1:的位置");
+        printf("%d\n",__builtin_ffs(x));
+        printf("左起第一個1之前0的個數:");
+        printf("%d\n",__builtin_clz(x));
+        printf("右起第一個1之後0的個數:");
+        printf("%d\n",__builtin_ctz(x));
+        printf("1的個數:");
+        printf("%d\n",__builtin_popcount(x));
+        printf("1的個數的奇偶性:");
+        printf("%d\n",__builtin_parity(x));
+    }
+    return 0;
+}
