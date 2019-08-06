10200b30a9d5c645178036527c28d40856846445
Kelly Lu <37441523+Kelly2336@users.noreply.github.com>
2019-08-07 00:20:40 +0800

    Update FenwickTree.cpp

--- Data Structure/FenwickTree.cpp
+++ Data Structure/FenwickTree.cpp
@@ -11,54 +11,54 @@ using namespace std;
 class BIT
 {
 private:
-	int sz = 0;
-	int d[maxn] = {0};
-	int dd[maxn] = {0};
-	inline int lowbit(int x)
-	{
-		return (-x)&x;
-	}
-	int prefix_sum(int x)
-	{
-		int ans1 = 0, ans2 = 0;
-		for(int i=x; i>0; i-=lowbit(i) )
-			ans1 += d[i], ans2 += dd[i];
-		return (x+1)*ans1 - ans2;
-	}
-	void update(int x, int v)
-	{
-		for(int i=x; i<=sz; i+=lowbit(i) )
-			d[i] += v, dd[i] += x*v;
-	}
+    int sz = 0;
+    int d[maxn] = {0};
+    int dd[maxn] = {0};
+    inline int lowbit(int x)
+    {
+        return (-x)&x;
+    }
+    int prefix_sum(int x)
+    {
+        int ans1 = 0, ans2 = 0;
+        for(int i=x; i>0; i-=lowbit(i) )
+            ans1 += d[i], ans2 += dd[i];
+        return (x+1)*ans1 - ans2;
+    }
+    void update(int x, int v)
+    {
+        for(int i=x; i<=sz; i+=lowbit(i) )
+            d[i] += v, dd[i] += x*v;
+    }
 public:
-	BIT(int n){sz = n;}
-	inline void add(int l,int r,int v)
-	{
-		update(l,v), update(r+1,-v);
-	}
-	inline int query(int l,int r)
-	{
-		return prefix_sum(r) - prefix_sum(l-1);
-	}
+    BIT(int n){sz = n;}
+    inline void add(int l,int r,int v)
+    {
+        update(l,v), update(r+1,-v);
+    }
+    inline int query(int l,int r)
+    {
+        return prefix_sum(r) - prefix_sum(l-1);
+    }
 };
 int main()
 {
-	int n;  cin >> n;
-	BIT bit(n);
-	string op;
-	while(cin >> op)
-	{
-		int l,r,v;
-		if(op == "add")
-		{
-			cin >> l >> r >> v;
-			bit.add(l,r,v);
-		}
-		else
-		{
-			cin >> l >> r;
-			cout << bit.query(l,r) << endl;
-		}
-	}
-	return 0;
+    int n;  cin >> n;
+    BIT bit(n);
+    string op;
+    while(cin >> op)
+    {
+        int l,r,v;
+        if(op == "add")
+        {
+            cin >> l >> r >> v;
+            bit.add(l,r,v);
+        }
+        else
+        {
+            cin >> l >> r;
+            cout << bit.query(l,r) << endl;
+        }
+    }
+    return 0;
 }
