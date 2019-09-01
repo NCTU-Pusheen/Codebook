782c8b7169061d762053a1d596a50ce43e1b78b5
NCTU-Pusheen <54011291+NCTU-Pusheen@users.noreply.github.com>
2019-09-01 14:33:23 +0800

    Create MinMeanCycle.cpp

---
+++ Graph/MinMeanCycle.cpp
@@ -0,0 +1,23 @@
+#include<cfloat> //for DBL_MAX
+int dp[MAXN][MAXN]; // 1-base,O(NM)
+vector<tuple<int,int,int>> edge;
+double mmc(int n){ //allow negative weight
+    const int INF = 0x3f3f3f3f;
+    for(int t=0; t<n; ++t){
+        memset(dp[t+1],0x3f,sizeof(dp[t+1]));
+        for(const auto &e:edge) {
+            int u,v,w;
+            tie(u,v,w) = e;
+            dp[t+1][v] = min(dp[t+1][v],dp[t][u]+w);
+        }
+    }
+    double res = DBL_MAX;
+    for(int u=1; u<=n; ++u) {
+        if(dp[n][u]==INF) continue;
+        double val = -DBL_MAX;
+        for(int t=0;t<n;++t)
+            val = max(val,(dp[n][u]-dp[t][u])*1.0/(n-t));
+        res = min(res,val);
+    }
+    return res;
+}
