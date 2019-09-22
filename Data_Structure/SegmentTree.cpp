7af73de527798be03a6ebaacaf02326536f700c1
Hyperbola <hyperbola.cs07@gmail.com>
2019-09-23 07:38:32 +0800

    Move tested BIT, Fenwick tree into SegmentTree file. Remove Luke's messy Segment Tree. Range update segment tree is vacant.

--- Data_Structure/SegmentTree.cpp
+++ Data_Structure/SegmentTree.cpp
@@ -1,101 +1,128 @@
-#define INF 2147483647
-struct Node {
-    int add_tag, chg_tag, Min, Max, sum;
-    Node():add_tag(0), chg_tag(0) {}
+// A binary indexed tree supports increasing (but not directly setting) specific value and querying range sum.
+// Time Complexity: O(NlogN)
+// SIMPLEST ONE
+class BIT {
+   private:
+    int n;
+    vector<ll> a;
+
+    ll query(int i) {
+        i++;
+        ll r = 0;
+        while (i > 0) r += a[i], i -= i & -i;
+        return r;
+    }
+
+   public:
+    // Constructs an empty binary indexed tree where n is size of array.
+    BIT(int n) : n(n) { a.resize(n + 1); }
+    // Increases element at i by v, where i in [1, n].
+    void add(int i, ll v) {
+        i++;
+        while (i <= n) a[i] += v, i += i & -i;
+    }
+    // Queries sum in [l, r], where l and r in [1, n].
+    ll query(int l, int r) { return query(r) - query(l - 1); }
 };
-struct ST {
-    int sz;
-    Node nd[maxn*4];
-    ST(){}
-    ST(int a[], int n):sz(n) {
-        build(a,0,n-1,1);
-    }
-    void build(int a[],int l,int r,int i) {
-        if(l==r) {
-            nd[i].Min = nd[i].Max = nd[i].sum = a[l];
-            return;
+
+// A Fenwick Tree supports increasing (but not directle setting) range values and querying range sum.
+// Time Complexity: (NlogN)
+// SUPPORT RANGE INCREASING
+class FenwickTree {
+   private:
+    int n;
+    vector<ll> d, dd;
+
+    ll query(int i) {
+        i++;
+        ll s = 0, ss = 0;
+        int c = i + 1;
+        while (i > 0) {
+            s += d[i], ss += dd[i];
+            i -= i & -i;
         }
-        int mid = (l+r)>>1;
-        int lid = i<<1, rid = (i<<1)|1;
-        build(a,l,mid,lid), build(a,mid+1,r,rid);
-        nd[i].Min = min(nd[lid].Min, nd[rid].Min);
-        nd[i].Max = max(nd[lid].Max, nd[rid].Max);
-        nd[i].sum = nd[lid].sum + nd[rid].sum;
+        return c * s - ss;
     }
-    void push(int l,int r,int i,int lid,int rid) {
-        if(nd[i].chg_tag) {
-            nd[i].sum = nd[i].chg_tag * (r-l+1);
-            nd[i].Max = nd[i].Min = nd[i].chg_tag;
-            if(lid) nd[lid].chg_tag = nd[i].chg_tag, nd[lid].add_tag = 0;
-            if(rid) nd[rid].chg_tag = nd[i].chg_tag, nd[rid].add_tag = 0;
-            nd[i].chg_tag = 0;
-        }
-        if(nd[i].add_tag) {
-            nd[i].sum += nd[i].add_tag * (r-l+1);
-            nd[i].Max += nd[i].add_tag, nd[i].Min += nd[i].add_tag;
-            if(lid) nd[lid].add_tag += nd[i].add_tag;
-            if(rid) nd[rid].add_tag += nd[i].add_tag;
-            nd[i].add_tag = 0;
+    void add(int i, ll v) {
+        i++;
+        int c = i;
+        while (i <= n) {
+            d[i] += v, dd[i] += c * v;
+            i += i & -i;
         }
     }
-    void add(int ql,int qr,int l,int r,int i,int val) {
-        if(!val) return;
-        int lid = i<<1, rid = (i<<1)|1;
-        if(l==r) lid = rid = 0;
-        push(l,r,i,lid,rid);
-        if(r<ql || qr<l) return;
-        if(ql<=l && r<=qr) {
-            nd[i].add_tag += val;
-        }
-        else {
-            int mid = (l+r)>>1;
-            add(ql,qr,l,mid,lid,val), add(ql,qr,mid+1,r,rid,val);
-            nd[i].Max = max(nd[lid].Max + nd[lid].add_tag, nd[rid].Max + nd[rid].add_tag);
-            nd[i].Min = min(nd[lid].Min + nd[lid].add_tag, nd[rid].Min + nd[rid].add_tag);
-            nd[i].sum = nd[lid].sum + nd[lid].add_tag*(mid-l+1) + nd[rid].sum + nd[rid].add_tag*(r-mid);
-        }
+
+   public:
+    // Constructs an empty Fenwick Tree where n is size of array.
+    FenwickTree(int n) : n(n) {
+        d.resize(n + 2);
+        dd.resize(n + 2);
     }
-    void chg(int ql,int qr,int l,int r,int i,int val) {
-        int lid = i<<1, rid = (i<<1)|1;
-        if(l==r) lid = rid = 0;
-        push(l,r,i,lid,rid);
-        if(r<ql || qr<l) return;
-        if(ql<=l && r<=qr) {
-            nd[i].chg_tag = val;
+    // Queries sum in [l, r], where l and r in [1, n].
+    ll query(int l, int r) { return query(r) - query(l - 1); }
+    // Increases all elements in [l, r] by v, where l and r in [1, n].
+    void add(int l, int r, ll v) { add(l, v), add(r + 1, -v); }
+};
+
+// A segment tree supports directly setting spcific value and querying range sum, min, max, product, gcd, lcm,
+// Time Complexity: (NlogN)
+// SUPPORT MANY TYPE OF QUERIES
+class SegmentTree {
+   private:
+    vector<ll> a;
+    int n;
+
+   private:
+    ll set(int i, int curl, int curr, int tar, ll v) {
+        if (curr < tar || curl > tar) return a[i];
+        if (curl == curr) return a[i] = v;
+        int m = (curl + curr) / 2;
+        ll l = set(i * 2 + 1, curl, m, tar, v);
+        ll r = set(i * 2 + 2, m + 1, curr, tar, v);
+        return a[i] = l + r;  // for sum query; replace with min(l,r), gcd(l,r), etc for other queries
+    }
+    ll query(int i, int curl, int curr, int tarl, int tarr) {
+        if (curr < tarl || curl > tarr) return 0;  // for sum, gcd query; replace with 1/INT_MAX/INT_MIN for product/min/max query
+        if (tarl <= curl && curr <= tarr) return a[i];
+        int m = (curl + curr) / 2;
+        ll l = query(i * 2 + 1, curl, m, tarl, tarr);
+        ll r = query(i * 2 + 2, m + 1, curr, tarl, tarr);
+        return l + r;  // for sum query; replace with min(l,r), gcd(l,r), etc for other queries
+    }
+
+   public:
+    // Constructs an empty segment tree with all values 0; n represeting size of elements
+    SegmentTree(int n) : n(n) { a.resize(n * 4); }
+    // Constructs a segment tree with initialized data, whose index starts at 1; n indicating size of data
+    SegmentTree(const vector<ll>& data, int n) : n(n) {
+        a.resize(n * 4);
+        init(data, 0, 0, n - 1);
+    }
+    // Sets element located at index i to value v, where i in [1, n]
+    void set(int i, ll v) {
+        i--;  // comment this line to 0-based
+        set(0, 0, n - 1, i, v);
+    }
+    // Queries range sum/min/max/gcd etc, whre l and r in [1, N]
+    ll query(int l, int r) {
+        l--, r--;  // comment this line to 0-based
+        return query(0, 0, n - 1, l, r);
+    }
+
+    // BELOWING CODES ARE OMITTABLE FOR EMPTY SEGMENT TREE
+   private:
+    ll init(const vector<ll>& data, int i, int curl, int curr) {
+        if (curl == curr) {
+            if (curl >= n)
+                return a[i] = 0;
+            else {
+                curl++;  // comment this line to 0-based
+                return a[i] = data[curl];
+            }
         }
-        else {
-            int mid = (l+r)>>1;
-            chg(ql,qr,l,mid,lid,val), chg(ql,qr,mid+1,r,rid,val);
-            nd[i].Max = max(nd[lid].chg_tag?nd[lid].chg_tag:nd[lid].Max, nd[rid].chg_tag?nd[rid].chg_tag:nd[rid].Max);
-            nd[i].Min = min(nd[lid].chg_tag?nd[lid].chg_tag:nd[lid].Min, nd[rid].chg_tag?nd[rid].chg_tag:nd[rid].Min);
-            nd[i].sum = (nd[lid].chg_tag?nd[lid].chg_tag*(mid-l+1):nd[lid].sum) + (nd[rid].chg_tag?nd[rid].chg_tag*(r-mid):nd[rid].sum);
-        }
-    }
-    int query_min(int ql,int qr,int l,int r,int i) {
-        int lid = i<<1, rid = (i<<1)|1;
-        if(l==r) lid = rid = 0;
-        push(l,r,i,lid,rid);
-        if(r<ql || qr<l) return INF;
-        if(ql<=l && r<=qr) return nd[i].Min;
-        int mid = (l+r)>>1;
-        return min(query_min(ql,qr,l,mid,lid), query_min(ql,qr,mid+1,r,rid));
-    }
-    int query_max(int ql,int qr,int l,int r,int i) {
-        int lid = i<<1, rid = (i<<1)|1;
-        if(l==r) lid = rid = 0;
-        push(l,r,i,lid,rid);
-        if(r<ql || qr<l) return -INF;
-        if(ql<=l && r<=qr) return nd[i].Max;
-        int mid = (l+r)>>1;
-        return max(query_max(ql,qr,l,mid,lid), query_max(ql,qr,mid+1,r,rid));
-    }
-    int query_sum(int ql,int qr,int l,int r,int i) {
-        int lid = i<<1, rid = (i<<1)|1;
-        if(l==r) lid = rid = 0;
-        push(l,r,i,lid,rid);
-        if(r<ql || qr<l) return 0;
-        if(ql<=l && r<=qr) return nd[i].sum;
-        int mid = (l+r)>>1;
-        return query_sum(ql,qr,l,mid,lid) + query_sum(ql,qr,mid+1,r,rid);
+        int m = (curl + curr) / 2;
+        ll l = init(data, i * 2 + 1, curl, m);
+        ll r = init(data, i * 2 + 2, m + 1, curr);
+        return a[i] = l + r;  // for sum query; replace with min(l,r), gcd(l,r), etc for other queries
     }
 };
