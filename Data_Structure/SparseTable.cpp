901c8da1c531f42e8597ad593da5e44823463fbb
Hyperbola <hyperbola.cs07@gmail.com>
2019-09-27 01:29:19 +0800

    Shorten codes

--- Data_Structure/SparseTable.cpp
+++ Data_Structure/SparseTable.cpp
@@ -1,21 +1,20 @@
 struct SparseTable {
-    const int N;
-    vector<vector<ll>> bitarray;
-    // Constructs a sparse table with size same as vec.
-    SparseTable(const vector<ll>& vec) : N(vec.size()) {
-        bitarray.assign(__lg(N) + 1, vector<ll>(N));
-        bitarray[0] = vec;
-        for (int i = 1; (1 << i) <= N; i++) {
-            int k = N - (1 << i);
+    vector<vector<int or ll>> a;
+    // Constructs a sparse table.
+    SparseTable(vector<int or ll>& data) {
+        int n = data.size();
+        a.assign(floor(log2(n)) + 1, vector<int or ll>(n));
+        a[0] = data;
+        for (int i = 1; (1 << i) <= n; i++) {
+            int k = n - (1 << i);
             for (int j = 0; j < k; j++) {
-                // Comparator may be needed.
-                bitarray[i][j] = max(bitarray[i - 1][j], bitarray[i - 1][j + (1 << (i - 1))]);
+                a[i][j] = max(a[i - 1][j], a[i - 1][j + (1 << (i - 1))]);
             }
         }
     }
-    // Queries the maximum element in [left_inc, right_inc] where left_inc and right_inc in [0, n).
-    ll query(int left_inc, int right_inc) {
-        int k = __lg(right_inc - left_inc + 1);
-        return max(bitarray[k][left_inc], bitarray[k][right_inc - (1 << k) + 1]);
+    // Queries the maximum element in [l, r] where 0- or 1-based are both safe.
+    ll maximal(int l, int r) {
+        int k = floor(log2(r - l + 1));
+        return max(a[k][l], a[k][r - (1 << k) + 1]);
     }
 };
