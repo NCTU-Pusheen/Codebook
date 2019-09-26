8491cf1ba6a405cd93b34e829d75ed4746f264a2
Hyperbola <hyperbola.cs07@gmail.com>
2019-09-27 01:33:05 +0800

    Shorten codes

--- Math/EulerFunction.cpp
+++ Math/EulerFunction.cpp
@@ -1,30 +1,30 @@
 // Queries phi(x) value.
 int phi(int x) {
-    int ret = x;
+    int r = x;
     for (int p = 2; p * p <= x; p++) {
         if (x % p == 0) {
             while (x % p == 0) x /= p;
-            ret -= ret / p;
+            r -= r / p;
         }
     }
-    if (x > 1) ret -= ret / x;
-    return ret;
+    if (x > 1) r -= r / x;
+    return r;
 }
 
 // Queries all phi(x) values where x in [0, n).
 vector<int> phi_in(int n) {
-    vector<bool> prime(n, 1);
-    vector<int> ret(n);
-    prime[0] = prime[1] = false;
-    for (int i = 0; i < n; i++) ret[i] = i;
+    vector<bool> p(n, 1);
+    vector<int> r(n);
+    p[0] = p[1] = 0;
+    for (int i = 0; i < n; i++) r[i] = i;
     for (int i = 2; i < n; i++) {
-        if (!prime[i]) continue;
-        ret[i]--;
+        if (!p[i]) continue;
+        r[i]--;
         for (int j = i * 2; j < n; j += i) {
-            prime[j] = false;
-            ret[j] = ret[j] / i * (i - 1);
+            p[j] = 0;
+            r[j] = r[j] / i * (i - 1);
         }
     }
-    ret[1] = 0;
-    return ret;
+    r[1] = 0;
+    return r;
 }
