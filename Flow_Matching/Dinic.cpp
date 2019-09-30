43062ba326fee1ea7a8bfee657e5d6520771f6e4
Hyperbola <hyperbola.cs07@gmail.com>
2019-09-30 21:53:45 +0800

    Remove reduntant codes

--- Flow_Matching/Dinic.cpp
+++ Flow_Matching/Dinic.cpp
@@ -1,16 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<double, double> pdd;
-const double PI = acos(-1);
-#define x first
-#define y second
-#define iter(c) c.begin(), c.end()
-#define ms(a) memset(a, 0, sizeof(a))
-#define mss(a) memset(a, -1, sizeof(a))
-#define mp(e, f) make_pair(e, f)
-
 /**
  * A dinic can find maximal count of flows in O(EV^2) in general, while in biparitite O(E * sqrt(v)). All arguments in this class
  * are safe for both 0- and 1-based cases without any changes.
