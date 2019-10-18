8d82ab4c7bc7a0b232235205456e80a2bc68de56
Hyperbola <hyperbola.cs07@gmail.com>
2019-10-18 21:50:38 +0800

    Kelly requested.....

--- Math/Discrete_sqrt.cpp
+++ Math/Discrete_sqrt.cpp
@@ -1,13 +1,3 @@
-// Queries (a ^ p) mod m.
-ll dpow(ll a, ll p, ll m) {
-    ll ret = 1;
-    while (p) {
-        if (p & 1) ret = ret * a % m;
-        a = a * a % m, p >>= 1;
-    }
-    return ret;
-}
-
 // Helper function for dsqrt
 int order(ll b, ll p) {
     if (__gcd(b, p) != 1) return -1;
@@ -52,24 +42,4 @@ ll dsqrt(ll y, ll p) {
         if (b == 1) return x;
         e = m;
     }
-}
-
-// Queries value x such that (ax == 1) mod p, where p is
-// prime and a is positive.
-ll modinv(ll a, ll p) {
-    if (p == 1) return 0;
-    ll pp = p, y = 0, x = 1;
-    while (a > 1) {
-        ll q = a / p, t = p;
-        p = a % p, a = t, t = y, y = x - q * y, x = t;
-    }
-    if (x < 0) x += pp;
-    return x;
-}
-
-// Queries value x such that (ax == b) mod p, where p is
-// prime and a, b are positive.
-ll modinv(ll a, ll b, ll p) {
-    ll ret = modinv(a, p);
-    return ret * b % p;
 }
