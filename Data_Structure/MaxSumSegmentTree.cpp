17e9828b9901c2b3dc5174170b409884b2bd79bd
Hyperbola <hyperbola.cs07@gmail.com>
2019-10-31 20:27:43 +0800

    完成驗證最大連續區間和線段樹

---
+++ Data_Structure/MaxSumSegmentTree.cpp
@@ -0,0 +1,59 @@
+#define ls i << 1
+#define rs i << 1 | 1
+class MaxSumSegmentTree {
+   private:
+    struct node {
+        ll lss, rss, ss, ans;
+        void set(ll v) {
+            lss = v, rss = v, ss = v, ans = v;
+        }
+    };
+    int n;
+    vector<node> a;
+    vector<ll> z;
+    void pull(int i) {
+        a[i].ss = a[ls].ss + a[rs].ss;
+        a[i].lss = max(a[ls].lss, a[ls].ss + a[rs].lss);
+        a[i].rss = max(a[rs].rss, a[rs].ss + a[ls].rss);
+        a[i].ans = max(max(a[ls].ans, a[rs].ans),
+                       a[ls].rss + a[rs].lss);
+    }
+    void build(int i, int l, int r) {
+        if (l == r) return a[i].set(z[l]), void();
+        int m = (l + r) >> 1;
+        build(ls, l, m), build(rs, m + 1, r), pull(i);
+    }
+    void set(int i, int l, int r, int q, ll v) {
+        if (l == r) return a[i].set(v), void();
+        int m = (l + r) >> 1;
+        if (q <= m)
+            set(ls, l, m, q, v);
+        else
+            set(rs, m + 1, r, q, v);
+        pull(i);
+    }
+    node query(int i, int l, int r, int ql, int qr) {
+        if (ql <= l && r <= qr) return a[i];
+        int m = (l + r) >> 1;
+        if (qr <= m) return query(ls, l, m, ql, qr);
+        if (m < ql) return query(rs, m + 1, r, ql, qr);
+        node lo = query(ls, l, m, ql, qr),
+             ro = query(rs, m + 1, r, ql, qr), ans;
+        ans.ss = lo.ss + ro.ss;
+        ans.lss = max(lo.lss, lo.ss + ro.lss);
+        ans.rss = max(ro.rss, ro.ss + lo.rss);
+        ans.ans = max(max(lo.ans, ro.ans), lo.rss + ro.lss);
+        return ans;
+    }
+
+   public:
+    void set(int i, ll v) { set(1, 1, n, i, v); }
+    ll query(int ql, int qr) {
+        return query(1, 1, n, ql, qr).ans;
+    }
+    MaxSumSegmentTree(int n) : n(n) {
+        a.resize(n << 2);
+        z.resize(n << 2);
+        build(1, 1, n);
+    }
+};
