f1102896ad3f88b79cbe593170467816ab879920
Hyperbola <hyperbola.cs07@gmail.com>
2019-11-01 10:45:11 +0800

    完成了比 mcmf 還快的 KM

--- Flow_Matching/KM.cpp
+++ Flow_Matching/KM.cpp
@@ -1,61 +1,58 @@
-#define MAXN 405
-#define INF 0x3f3f3f3f3f3f3f3f
-int n;// 1-base，0表示沒有匹配 
-LL g[MAXN][MAXN]; //input graph
-int My[MAXN],Mx[MAXN]; //output match
-LL lx[MAXN],ly[MAXN],pa[MAXN],Sy[MAXN];
-bool vx[MAXN],vy[MAXN];
-void augment(int y){
-    for(int x, z; y; y = z) {
-        x = pa[y], z = Mx[x];
-        My[y] = x, Mx[x] = y;
-    }
-}
-void bfs(int st){
-    for(int i=1; i<=n; ++i)
-        Sy[i] = INF, vx[i]=vy[i]=0;
-    queue<int> q; q.push(st);
-    while(1) {
-        while(q.size()) {
-            int x=q.front(); q.pop();
-            vx[x]=1;
-            for(int y=1; y<=n; ++y) if(!vy[y]){
-                LL t = lx[x]+ly[y]-g[x][y];
-                if(t==0) {
-                    pa[y]=x;
-                    if(!My[y]) {augment(y);return;}
-                    vy[y]=1,q.push(My[y]);
+/** 二分圖最大權值匹配 KM 演算法，複雜度 O(n^3)*/
+#define inf 5e18
+struct KM {
+    const vector<vector<ll>>& e;
+    int xx, yy;
+    vector<ll> cx, cy, wx, wy;
+    vector<bool> vx, vy;
+    ll z;
+
+    bool dfs(int u) {
+        vx[u] = 1;
+        for (int v = 0; v < yy; v++) {
+            if (vy[v] || e[u][v] == inf) continue;
+            ll t = wx[u] + wy[v] - e[u][v];
+            if (t == 0) {
+                vy[v] = 1;
+                if (cy[v] == -1 || dfs(cy[v])) {
+                    cx[u] = v, cy[v] = u;
+                    return 1;
                 }
-                else if(Sy[y]>t) pa[y]=x,Sy[y]=t;
+            } else if (t > 0)
+                z = min(z, t);
+        }
+        return 0;
+    }
+
+    ll max_weight() {
+        for (int i = 0; i < xx; i++)
+            for (int j = 0; j < yy; j++) {
+                if (e[i][j] == inf) continue;
+                wx[i] = max(wx[i], e[i][j]);
+            }
+
+        for (int i = 0; i < xx; i++) {
+            while (1) {
+                z = inf, vx.assign(xx, 0), vy.assign(yy, 0);
+                if (dfs(i)) break;
+                for (int j = 0; j < xx; j++)
+                    if (vx[j]) wx[j] -= z;
+                for (int j = 0; j < yy; j++)
+                    if (vy[j]) wy[j] += z;
             }
         }
-        LL cut = INF;
-        for(int y=1; y<=n; ++y)
-            if(!vy[y]&&cut>Sy[y]) cut=Sy[y];
-        for(int j=1; j<=n; ++j) {
-            if(vx[j]) lx[j] -= cut;
-            if(vy[j]) ly[j] += cut;
-            else Sy[j] -= cut;
-        }
-        for(int y=1; y<=n; ++y) {
-            if(!vy[y]&&Sy[y]==0) {
-                if(!My[y]){augment(y);return;}
-                vy[y]=1, q.push(My[y]);
-            }
-        }
+
+        ll ans = 0;
+        for (int i = 0; i < xx; i++)
+            if (cx[i] != -1) ans += e[i][cx[i]];
+        return ans;
     }
-}
-LL KM() {
-    memset(My,0,sizeof(int)*(n+1));
-    memset(Mx,0,sizeof(int)*(n+1));
-    memset(ly,0,sizeof(LL)*(n+1));
-    for(int x=1; x<=n; ++x){
-        lx[x] = -INF;
-        for(int y=1; y<=n; ++y)
-            lx[x] = max(lx[x],g[x][y]);
+
+    // 給他 n*m 的權重表 (n>=m) ，求最大完全匹配權重
+    // n<m 會無窮迴圈
+    KM(vector<vector<ll>> e) : e(e) {
+        xx = e.size(), yy = e[0].size(); // xx 要 >= yy !!
+        cx.assign(xx, -1), cy.assign(yy, -1);
+        wx.assign(xx, 0), wy.assign(yy, 0);
     }
-    for(int x=1; x<=n; ++x) bfs(x);
-    LL ans = 0;
-    for(int y=1; y<=n; ++y) ans += g[My[y]][y];
-    return ans;
-}
+};
