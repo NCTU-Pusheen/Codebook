1d4ebb08e44c035c791a9318db05a75e45ff46cb
Hyperbola <hyperbola.cs07@gmail.com>
2019-11-15 23:26:15 +0800

    Remove reduntant code in Math/Expression.cpp

--- Math/Expression.cpp
+++ Math/Expression.cpp
@@ -1,16 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<double, double> pdd;
-const double PI = acos(-1);
-#define x first
-#define y second
-#define iter(c) c.begin(), c.end()
-#define ms(a) memset(a, 0, sizeof(a))
-#define mss(a) memset(a, -1, sizeof(a))
-#define mp(e, f) make_pair(e, f)
-
 /**
  * 支援處理四則運算的工具。給四則運算的字串，檢查格式並計算其值。如果
  * 格式不合法，會丟出錯誤。複雜度 O(字串長度) 。支援的符號有四則運算
@@ -19,7 +6,7 @@ const double PI = acos(-1);
  * 視為 -1) 。空字串視為不合法。運算範圍限於 long long 。如果試圖除
  * 以零或對零求餘也會丟出錯誤。
  */
-void req(bool b) { if (!b) throw 87; }
+void req(bool b) { if (!b) throw ""; }
 const int B = 2; // 可以調整成 B 進位
 class Expr {
    private:
@@ -30,34 +17,38 @@ class Expr {
         return src.empty() ? '\0' : src.front();
     }
     inline char pop() {
-        char c = top(); src.pop_front(); return c;
+        char c = src.front(); src.pop_front(); return c;
     }
     ll n() {
         ll ret = pop() - '0';
         // 若要禁止數字以 0 開頭，加上這行
-        req(ret || !isdigit(top()));
+        // req(ret || !isdigit(top()));
         while (isdigit(top())) ret = B * ret + pop() - '0';
         return ret;
     }
     ll fac() {
         if (isdigit(top())) return n();
-        if (top() == '-') return pop(), -fac();
+        if (top() == '-') { pop(); return -fac(); }
         if (top() == '(') {
             pop();
-            int ret = expr(1);
-            return req(pop() == ')'), ret;
+            ll ret = expr(1);
+            req(pop() == ')');
+            return ret;
         }
         // 若要允許前置正號，加上這行
-        // else if(top() == '+') { pop(); return fac(); }
-        return req(0), 0;
+        // if(top() == '+') { pop(); return fac(); }
+        throw "";
     }
     ll term() {
-        ll ret = fac(); char c;
-        while ((c = top()) && (c == '*' || c == '/' || c == '%')) {
+        ll ret = fac(); char c = top();
+        while (c == '*' || c == '/' || c == '%') {
             pop();
-            if (c == '*') { ret *= fac(); continue; }
-            ll t = fac(); req(t);
-            if (c == '/') ret /= t; else ret %= t;
+            if (c == '*') ret *= fac();
+            else {
+                ll t = fac(); req(t);
+                if (c == '/') ret /= t; else ret %= t;
+            }
+            c = top();
         }
         return ret;
     }
@@ -66,7 +57,8 @@ class Expr {
         while (top() == '+' || top() == '-')
             if (pop() == '+') ret += term();
             else ret -= term();
-        return req(top() == (k ? ')' : '\0')), ret;
+        req(top() == (k ? ')' : '\0'));
+        return ret;
     }
 
    public:
