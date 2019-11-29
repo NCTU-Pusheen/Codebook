7228f6407c5340db1e0f977f76a1c6cbd38db660
Hyperbola <hyperbola.cs07@gmail.com>
2019-11-29 20:51:46 +0800

    簡化 Rolling hash

--- String/RollHash.cpp
+++ String/RollHash.cpp
@@ -1,16 +1,22 @@
-// 問 sub 在 str 第一次出現的開頭 index 。-1 表示找不到。
+// 問 pat 在 str 第一次出現的開頭 index 。-1 表示找不到。
 int rollhash(string& str, string& pat) {
-    const int x = 1e6 + 99, m = 1e9 + 9; // 隨意大質數
+    const ll x = 1e6 + 99;  // 隨意大質數，建議 1e6
+    const ll m = 1e9 + 9;   // 隨意大質數，建議 1e9
+    assert(pat.size());     // pat 不能是空字串
     ll xx = 1, sh = 0;
-    for (int i = 0; i < pat.size(); i++) xx = xx * x % m;
-    for (char c : pat) sh = (sh * x + c) % m;
-    vector<ll> hash = {0};
-    for (int i = 0; i < str.size(); i++) {
-        hash.push_back((hash.back() * x + str[i]) % m);
-        if (i < pat.size()) continue;
-        ll h = hash.back() - hash[i - pat.size() + 1] * xx;
+    for (char c : pat)
+        sh = (sh * x + c) % m, xx = xx * x % m;
+
+    deque<ll> hash = {0};
+    int ret = 0;
+    for (char c : str) {
+        hash.push_back((hash.back() * x + c) % m);
+        if (hash.size() <= pat.size()) continue;
+        ll h = hash.back() - hash.front() * xx;
         h = (h % m + m) % m;
-        if (h == sh) return i - pat.size() + 1;
+        if (h == sh) return ret;
+        hash.pop_front();
+        ret++;
     }
     return -1;
 }
