7715c3dd34119234cfd5ed8f5c569319a4490dbe
Hyperbola <hyperbola.cs07@gmail.com>
2019-12-04 02:04:45 +0800

    完成 Treap

--- Data_Structure/Treap.cpp
+++ Data_Structure/Treap.cpp
@@ -1,181 +1,153 @@
-//POJ 3580 區間反轉＋加值 詢問min
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define maxn 100005
-#define INF 2147483647
-struct Treap
-{
-    Treap *L, *R;
-    int min_val, size, val;
-    bool rev_tag;
-    int add_tag;
-    Treap(int _val):L(NULL),R(NULL),min_val(_val),size(1),rev_tag(false),add_tag(0),val(_val){}
-    void pull()
-    {
-        if(L) L->push();
-        if(R) R->push();
-        size =  (L ? L->size : 0) + (R ? R->size : 0) + 1;
-        min_val = min( min( val, L ? L->min_val : INF), R ? R->min_val : INF);
+typedef long long ll;
+
+// 支援區間加值、區間反轉、區間 rotate 、區間刪除、插入元素、求區間
+// 最小值的元素的 Treap。使用前建議 srand(time(0)); 除了 size()
+// 方法以外，所有操作都是 O(log N) 。所有 public 方法各自獨立，請
+// 斟酌要使用到哪些方法，有需要的才抄。
+class Treap {
+   private:
+    struct Node {
+        int pri = rand(), size = 1;
+        ll val, mn, inc = 0;
+        bool rev = 0;
+        Node *lc = 0, *rc = 0;
+        Node(ll v) { val = mn = v; }
+    };
+    Node* root = 0;
+
+    void rev(Node* t) {
+        if (!t) return;
+        swap(t->lc, t->rc), t->rev ^= 1;
     }
-    void push()
-    {
-        val += add_tag;
-        min_val += add_tag;
-        if(L) L->add_tag += add_tag;
-        if(R) R->add_tag += add_tag;
-        add_tag = 0;
-        if(rev_tag)
-        {
-            swap(L,R);
-            if(L) L->rev_tag ^= 1;
-            if(R) R->rev_tag ^= 1;
-            rev_tag = false;
+
+    void update(Node* t, ll v) {
+        if (!t) return;
+        t->val += v, t->inc += v, t->mn += v;
+    }
+
+    void push(Node* t) {
+        if (t->rev) rev(t->lc), rev(t->rc), t->rev = 0;
+        update(t->lc, t->inc), update(t->rc, t->inc);
+        t->inc = 0;
+    }
+
+    void pull(Node* t) {
+        t->size = 1 + size(t->lc) + size(t->rc);
+        t->mn = t->val;
+        if (t->lc) t->mn = min(t->mn, t->lc->mn);
+        if (t->rc) t->mn = min(t->mn, t->rc->mn);
+    }
+
+    // 看你要不要釋放記憶體
+    void discard(Node* t) {
+        if (!t) return;
+        discard(t->lc), discard(t->rc);
+        delete t;
+    }
+
+    void split(Node* t, Node*& a, Node*& b, int k) {
+        if (!t) return a = b = 0, void();
+        push(t);
+        if (size(t->lc) < k) {
+            a = t;
+            split(t->rc, a->rc, b, k - size(t->lc) - 1);
+            pull(a);
+        } else {
+            b = t;
+            split(t->lc, a, b->lc, k);
+            pull(b);
         }
+    }
+
+    Node* merge(Node* a, Node* b) {
+        if (!a || !b) return a ? a : b;
+        if (a->pri > b->pri) {
+            push(a);
+            a->rc = merge(a->rc, b);
+            pull(a);
+            return a;
+        } else {
+            push(b);
+            b->lc = merge(a, b->lc);
+            pull(b);
+            return b;
+        }
+    }
+
+    inline int size(Node* t) { return t ? t->size : 0; }
+
+   public:
+    // 問此 Treap 的元素數量，複雜度 O(1)
+    int size() { return size(root); }
+
+    // 令區間 [l, r] 增值 val ，複雜度 O(log n)
+    void add(int l, int r, ll val) {
+        Node *a, *b, *c, *d;
+        split(root, a, b, r);
+        split(a, c, d, l - 1);
+        update(d, val);
+        root = merge(merge(c, d), b);
+    }
+
+    // 反轉區間 [l, r] ，複雜度 O(log n)
+    void reverse(int l, int r) {
+        Node *a, *b, *c, *d;
+        split(root, a, b, r);
+        split(a, c, d, l - 1);
+        swap(d->lc, d->rc);
+        d->rev ^= 1;
+        root = merge(merge(c, d), b);
+    }
+
+    // 區間 [l, r] 向右 rotate k 次， k < 0 表向左 rotate
+    // 複雜度 O(log n)
+    void rotate(int l, int r, int k) {
+        int len = r - l + 1;
+        Node *a, *b, *c, *d, *e, *f;
+        split(root, a, b, r);
+        split(a, c, d, l - 1);
+        k = (k + len) % len;
+        split(d, e, f, len - k);
+        root = merge(merge(c, merge(f, e)), b);
+    }
+
+    // 插入一個元素 val 使其 index = i ，複雜度 O(log n)
+    // 注意 i <= size
+    void insert(int i, ll val) {
+        if (i == size() + 1) {
+            push_back(val);
+            return;
+        }
+        assert(i <= size());
+        Node *a, *b;
+        split(root, a, b, i - 1);
+        root = merge(merge(a, new Node(val)), b);
+    }
+
+    // 添加元素至尾端，複雜度 O(log n)
+    void push_back(ll val) {
+        root = merge(root, new Node(val));
+    }
+
+    // 刪除區間 [l, r] 的元素，複雜度 O(log n)
+    void remove(int l, int r) {
+        int len = r - l + 1;
+        Node *a, *b, *c, *d;
+        split(root, a, b, l - 1);
+        split(b, c, d, len);
+        discard(c); // 看你要不要釋放記憶體
+        root = merge(a, d);
+    }
+
+    // 問區間 [l, r] 的最小值，複雜度 O(log n)
+    ll minn(int l, int r) {
+        Node *a, *b, *c, *d;
+        split(root, a, b, r);
+        split(a, c, d, l - 1);
+        int ans = d->mn;
+        root = merge(merge(c, d), b);
+        return ans;
     }
 };
-
-inline int Random()
-{
-    static int x = 7122;
-    return (x = x * 0xdefaced + 1) & INF;
-}
-
-void split(Treap *p, Treap *&a, Treap *&b, int k)
-{
-    int sz = p ? p->size : 0;
-    if(!p) a = b = NULL;
-    else if(k<=0) a = NULL, b = p;
-    else
-    {
-        p->push();
-        if(p->L && p->L->size >=k)
-        {
-            b = p;
-            split(p->L, a, b->L, k);
-        }
-        else
-        {
-            a = p;
-            split(p->R, a->R, b, k - (p->L ? p->L->size : 0) -1 );
-        }
-        p->pull();
-    }
-}
-Treap* merge(Treap *a, Treap *b)
-{
-    if(!a) return b;
-    if(!b) return a;
-    if(Random()%(a->size+b->size) < a->size)
-    {
-        a->push();
-        a->R = merge(a->R,b);
-        a->pull();
-        return a;
-    }
-    b->push();
-    b->L = merge(a,b->L);
-    b->pull();
-    return b;
-}
-void insert(Treap *&p, int x, int pos)
-{
-    Treap *a, *b;
-    Treap *t = new Treap(x);
-    split(p,a,b,pos);
-    p = merge(merge(a,t),b);
-}
-void del(Treap *&p, int x)
-{
-    Treap *a, *b, *c, *d;
-    split(p,a,d,x-1);
-    split(d,b,c,1);
-    p = merge(a,c);
-}
-void reverse(Treap *&p, int x,int y)
-{
-    Treap *a, *b ,*c;
-    split(p,a,c,y);
-    split(a,a,b,x-1);
-    b->rev_tag ^= 1;
-    p = merge(merge(a,b),c);
-}
-void add(Treap *&p, int x,int y,int v)
-{
-    Treap *a, *b ,*c;
-    split(p,a,c,y);
-    split(a,a,b,x-1);
-    b->add_tag += v;
-    p = merge(merge(a,b),c);
-}
-int Min(Treap *&p, int x, int y)
-{
-    Treap *a, *b, *c;
-    split(p,a,c,y);
-    split(a,a,b,x-1);
-    int ans = b->min_val + b->add_tag;
-    p = merge(merge(a,b),c);
-    return ans;
-}
-void revolve(Treap *&p, int x, int y, int t)
-{
-    Treap *a, *b ,*c, *d ,*e;
-    split(p,a,c,y);
-    split(a,a,b,x-1);
-    split(b,d,e,(y-x+1)-t%(y-x+1));
-    p = merge(merge(a,merge(e,d)),c);
-}
-
-int main()
-{
-    int n,m;
-    scanf("%d",&n);
-    int a[maxn];
-    for(int i=1; i<=n; i++)
-        scanf("%d",&a[i]);
-    Treap* root = new Treap(a[1]);
-    for(int i=2; i<=n; i++)
-        insert(root,a[i],i);
-    scanf("%d",&m);
-    while(m--)
-    {
-        char s[10];
-        scanf(" %s",s);
-        if(strcmp(s,"ADD")==0)
-        {
-            int x, y, d;
-            scanf("%d%d%d",&x,&y,&d);
-            add(root,x,y,d);
-        }
-        else if(strcmp(s,"REVERSE")==0)
-        {
-            int x, y;
-            scanf("%d%d",&x,&y);
-            reverse(root,x,y);
-        }
-        else if(strcmp(s,"REVOLVE")==0)
-        {
-            int x, y, t;
-            scanf("%d%d%d",&x,&y,&t);
-            revolve(root,x,y,t);
-        }
-        else if(strcmp(s,"INSERT")==0)
-        {
-            int x, p;
-            scanf("%d%d",&x,&p);
-            insert(root,p,x);
-        }
-        else if(strcmp(s,"DELETE")==0)
-        {
-            int x;
-            scanf("%d",&x);
-            del(root,x);
-        }
-        else if(strcmp(s,"MIN")==0)
-        {
-            int x, y;
-            scanf("%d%d",&x,&y);
-            printf("%d\n",Min(root,x,y));
-        }
-    }
-    return 0;
-}
