9f82ef828942eaba0d24d8df8740eaff0a5ba943
hyperbola <hyperbola.cs07@gmail.com>
2020-09-24 13:51:53 +0800

    Update stable marriage

--- Flow_Matching/Stable_Marriage.cpp
+++ Flow_Matching/Stable_Marriage.cpp
@@ -1,54 +1,29 @@
-#pragma GCC optimize("Ofast")
-#include <bits/stdc++.h>
+const int INF = 2e9;
+const int N = ? ;       // 男女總人數；女性 index 0 ~ p ，男性 index p+1 ~ N-1
+int vis[N], rnd, m[N];  // 跑完匈牙利之後配對結果儲存於此， -1 表示人醜
+vector<int> g[N];       // 關係表
 
-using namespace std;
-
-vector<vector<int>> edges;
-vector<bool> visit;
-vector<int> match;
-
-
-bool dfs(int from) {
-    for(int next : edges[from]) {
-        if(visit[next]) continue;
-        visit[next] = 1;
-        if(match[next] == -1 || dfs(match[next])) {
-            match[next] = from;
-            match[from] = next;
-            return true;
+int dfs(int s) {
+    for (int x : g[s]) {
+        if (vis[x]) continue;
+        vis[x] = 1;
+        if (m[x] == -1 || dfs(m[x])) {
+            m[x] = s, m[s] = x;
+            return 1;
         }
     }
-    return false;
+    return 0;
 }
 
-int hungarian(int p, int q) {
-    int ans = 0;
-
-    for(int i = 0;i < p;i++) {
-        if(match[i] == -1) {
-            visit.assign(visit.size(), 0);
-            if(dfs(i)) ans += 1;
+// 回傳成功結婚對數
+int hungarian(int p) {  // 傳入女性人數
+    memset(m, -1, sizeof(m));
+    int c = 0;
+    for (int i = 0; i < p; i++) {
+        if (m[i] == -1) {
+            memset(vis, 0, sizeof(vis));
+            c += dfs(i);
         }
     }
-    return ans;
-}
-
-int main() {
-    ios::sync_with_stdio(false) ,cin.tie(0) ,cout.tie(0);
-    int p, q, k; // 有 p 個紅點， q 個藍點， 和 k 條邊 
-    cin >> p >> q >> k;
-    edges.resize(p + 5);
-    match.resize(p + q + 5, -1);
-    visit.resize(p + q + 5, false);
-    for(int i = 0; i < k; i++) {
-        int eax, ebx;
-        cin >> eax >> ebx;
-        eax --; ebx --;
-        ebx += p;
-        edges[eax].push_back(ebx);
-    }
-    cout << hungarian(p, q) << endl;
-    edges.clear();
-    visit.clear();
-    match.clear();
+    return c;
 }
