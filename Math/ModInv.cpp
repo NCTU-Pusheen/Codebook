befde4a9bd74a7bcb864b224c0a4e7e18db2d6ae
Chi-Yuan Chen <157-cychen2602@users.noreply.git.cs.nctu.edu.tw>
2020-10-18 11:20:03 +0800

    支持非質數模逆元計算

--- Math/ModInv.cpp
+++ Math/ModInv.cpp
@@ -1,3 +1,34 @@
+// 解 (ax == 1) mod b。a、b必須是互質整數，否則不存在mod inverse。
+int phi(int x) {
+    int r = x;
+    for (int p = 2; p * p <= x; p++) {
+        if (x % p == 0) {
+            while (x % p == 0) x /= p;
+            r -= r / p;
+        }
+    }
+    if (x > 1) r -= r / x;
+    return r;
+}
+
+ll modinv(ll a, ll b){
+	if(__gcd(a, b) != 1){
+		return -1;
+	}
+	// Euler 定理: a^phi(b) == 1 (mod b)
+	// -> a^(phi(b) - 1) is the mod inverse to b of a
+	int mod_inv_pow = phi(b) - 1;
+	int ans = 1, base = a % b;
+	while(mod_inv_pow > 0){
+		if(mod_inv_pow & 1){
+			ans = ans * base % b;
+		}
+		base = base * base % b;
+		mod_inv_pow >>= 1;
+	}
+	return ans;
+}
+
 // 解 (ax == 1) mod p 。p 必須是質數，a 是正整數。
 ll modinv(ll a, ll p) {
     if (p == 1) return 0;
