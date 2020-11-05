354616d93607a8ebf2c6b95e634aabd1da061b2b
hyperbola <hyperbola.cs07@gmail.com>
2020-11-06 02:35:44 +0800

    新的 FFT 模板 (卦長語法小改)

--- Math/FFT.cpp
+++ Math/FFT.cpp
@@ -1,30 +1,37 @@
-template<typename T,typename VT=vector<complex<T> > >
-struct FFT{
-    const T pi;
-    FFT(const T pi=acos((T)-1)):pi(pi){}
-    unsigned bit_reverse(unsigned a,int len){
-        a=((a&0x55555555U)<<1)|((a&0xAAAAAAAAU)>>1);
-        a=((a&0x33333333U)<<2)|((a&0xCCCCCCCCU)>>2);
-        a=((a&0x0F0F0F0FU)<<4)|((a&0xF0F0F0F0U)>>4);
-        a=((a&0x00FF00FFU)<<8)|((a&0xFF00FF00U)>>8);
-        a=((a&0x0000FFFFU)<<16)|((a&0xFFFF0000U)>>16);
-        return a>>(32-len);
+const double PI = acos(-1);
+typedef complex<double> C;
+namespace FFT {
+
+void bit_reverse(vector<C>& v) {
+    int n = v.size();
+    for (int i = 1, j = n / 2; i < n - 1; i++) {
+        if (i < j) swap(v[i], v[j]);
+        int k = n / 2;
+        while (j >= k) j -= k, k /= 2;
+        if (j < k) j += k;
     }
-    void fft(bool is_inv,VT &in,VT &out,int N){
-        int bitlen=__lg(N),num=is_inv?-1:1;
-        for(int i=0;i<N;++i) out[bit_reverse(i,bitlen)]=in[i];
-        for(int step=2; step<=N; step<<=1){
-            const int mh = step>>1;
-            for(int i=0; i<mh; ++i){
-                complex<T> wi = exp(complex<T>(0,i*num*pi/mh));
-                for(int j=i; j<N; j+=step){
-                    int k = j+mh;
-                    complex<T> u = out[j], t = wi*out[k];
-                    out[j] = u+t;
-                    out[k] = u-t;
-                }
+}
+
+// Given a function whose co-efficient is v, calculates its FFT and stores
+// resulting co-efficients in local. Length of v MUST be 2^N.
+void fft(vector<C>& v, bool is_inverse = false) {
+    int n = v.size(), len = __lg(n), f = is_inverse ? -1 : 1;
+    assert(n == (1 << len));
+    bit_reverse(v);
+    for (int k = 2; k <= n; k <<= 1) {
+        int m = k >> 1;
+        for (int i = 0; i < m; i++) {
+            C c = {0, i * f * PI / m};
+            auto w = exp(c);
+            for (int j = i; j < n; j += k) {
+                int k = j + m;
+                C s = v[j], t = w * v[k];
+                v[j] = s + t, v[k] = s - t;
             }
         }
-        if(is_inv) for(int i=0;i<N;++i) out[i]/=N;
     }
-};
+    if (is_inverse)
+        for (int i = 0; i < n; ++i) v[i] /= n;
+}
+
+};  // namespace FFT
