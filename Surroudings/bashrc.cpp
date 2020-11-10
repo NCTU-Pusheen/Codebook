f5db0fbe6b88d767e844cc108376b78167fae4e1
Lin2602 <30391069+LeeLin2602@users.noreply.github.com>
2020-11-10 15:57:09 +0800

    Update bashrc.cpp
    經測試可用

--- Surroudings/bashrc.cpp
+++ Surroudings/bashrc.cpp
@@ -1,9 +1,3 @@
 oj() {
-  ext=${1##*.}            #空格敏感
-  filename=${1##*/}       #空格敏感
-  filename=${filename%.*} #空格敏感
-  case $ext in
-    cpp ) g++ -o "/tmp/$filename" "$1" && "/tmp/$filename" ;; #空格不敏感
-    py  ) python3 "$1" ;;                                     #空格不敏感
-  esac
+  g++ -o "/tmp/out" "$1" && "/tmp/out"
 }
