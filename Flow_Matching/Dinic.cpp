ade20b4ddbf0c65f7c5b62b995f35ae06d7ed044
hyperbola <hyperbola.cs07@gmail.com>
2020-11-22 03:16:15 +0800

    Dinic 簡化語法、壓行數

--- Flow_Matching/Dinic.cpp
+++ Flow_Matching/Dinic.cpp
@@ -1,20 +1,17 @@
 class Dinic {
-    struct edge {
-        int d, r; ll c;
-        edge(int d, ll c, int r) : d(d), c(c), r(r){};
-    };
    private:
+    struct edge { int d, r; ll c; };
     vector<vector<edge>> adj; vector<int> lv, ve; int n;
     bool mklv(int s, int d) {
-        lv.assign(n, -1); lv[s] = 0;
-        queue<int> q; q.push(s);
+        lv.assign(n, -1); lv[s] = 0; queue<int> q({s});
         while (!q.empty()) {
             int v = q.front(); q.pop();
             for (auto& e : adj[v]) {
                 if (e.c == 0 || lv[e.d] != -1) continue;
                 lv[e.d] = lv[v] + 1, q.push(e.d);
             }
-        } return lv[d] > 0;
+        }
+        return lv[d] > 0;
     }
     ll aug(int v, ll f, int d) {
         if (v == d) return f;
@@ -23,26 +20,27 @@ class Dinic {
             if (lv[e.d] != lv[v] + 1 || !e.c) continue;
             ll sent = aug(e.d, min(f, e.c), d);
             if (sent > 0) {
-                e.c -= sent, adj[e.d][e.r].c += sent;
+                e.c -= sent, adj[e.d][e.r].c += sent; 
                 return sent;
             }
-        } return 0;
+        } 
+        return 0;
     }
    public:
-    // 建空圖， n 節點數 (含 source, sink)
+    // 建空圖。 n 為節點數量 (含 source 和 sink)。
     Dinic(int n) : n(n + 1) { clear(); }
-    void clear() { adj.assign(n, vector<edge>()); }
-    // 加有向邊 src->dst ，cap 是容量
+    void clear() { adj.assign(n, {}); }
     void add_edge(int src, int dst, ll cap) {
-        edge ss(dst, cap, adj[dst].size());
-        edge dd(src, 0, adj[src].size());
+        edge ss{dst, (int)adj[dst].size(), cap};
+        edge dd{src, (int)adj[src].size(), 0};
         adj[src].push_back(ss), adj[dst].push_back(dd);
     }
     ll max_flow(int s, int d) {
         ll ret = 0;
         while (mklv(s, d)) {
             ve.assign(n, 0);
-            while (ll f = aug(s, 9e18, d)) ret += f;
-        } return ret;
+            while (ll f = aug(s, inf, d)) ret += f;
+        } 
+        return ret;
     }
 };
