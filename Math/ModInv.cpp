7a1e0708b18e6ec8938e71d8c5120f4204cb0f36
Lin2602 <LeeLin2602@gmail.com>
2021-04-23 06:54:57 +0800

    Update ModInv.cpp

--- Math/ModInv.cpp
+++ Math/ModInv.cpp
@@ -38,3 +38,11 @@ ll modinv(ll a, ll b, ll p) {
     ll ret = modinv(a, p);
     return ret * b % p;
 }
+
+// O(n) 求 1...n 對 p 的模逆元
+ll precalc(int n, ll p){
+    for(int i = 1; i <= n; i++){
+        if(i == 1) inv[i] = 1;
+        else inv[i] = (p - (p / i) * inv[p % i] % p) % p;
+    }
+}
