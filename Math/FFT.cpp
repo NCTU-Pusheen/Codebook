34bc571297a1af3ae082ae4c77e474600a2b223d
Hyperbola <me@hyperbola.me>
2021-10-26 17:53:13 +0800

    update ftt

--- Math/FFT.cpp
+++ Math/FFT.cpp
@@ -1,43 +1,41 @@
-// int(complex.real() + 0.05)   // .imag()
-template <typename T, typename VT = vector<complex<T>>>
-struct FFT {
-    const T pi;
-    FFT(const T pi = acos((T)-1.0)) : pi(pi) {}
-    unsigned bit_reverse (unsigned a, int len) {
-        a = ((a&0x55555555U)<<1)  | ((a&0xAAAAAAAAU)>>1);
-        a = ((a&0x33333333U)<<2)  | ((a&0xCCCCCCCCU)>>2);
-        a = ((a&0x0F0F0F0FU)<<4)  | ((a&0xF0F0F0F0U)>>4);
-        a = ((a&0x00FF00FFU)<<8)  | ((a&0xFF00FF00U)>>8);
-        a = ((a&0x0000FFFFU)<<16) | ((a&0xFFFF0000U)>>16);
-        return a >> (32-len);
-    }
-    void fft (bool is_inv, VT &in, VT &out, int N) {
-        int bitlen = __lg(N), num = is_inv ? -1 : 1;
-        for(int i = 0; i < N; ++i)
-            out[bit_reverse(i, bitlen)] = in[i];
-        for(int step = 2, mh = 1; step <= N; step <<= 1, mh <<= 1){
-            for(int i = 0; i < mh; ++i){
-                complex<T> wi = exp(complex<T>(0, i * num * pi / mh));
-                for(int j = i, k = i + mh; j < N; j += step, k += step) {
-                    complex<T> u = out[j], t = wi * out[k];
-                    out[j] = u + t, out[k] = u - t;
-                }
+typedef unsigned u;
+typedef complex<double> c;
+const double PI = acos(-1.0);
+u bit_rev(u a, int len) {
+    a = ((a & 0x55555555U) << 1)  | ((a & 0xAAAAAAAAU) >> 1);
+    a = ((a & 0x33333333U) << 2)  | ((a & 0xCCCCCCCCU) >> 2);
+    a = ((a & 0x0F0F0F0FU) << 4)  | ((a & 0xF0F0F0F0U) >> 4);
+    a = ((a & 0x00FF00FFU) << 8)  | ((a & 0xFF00FF00U) >> 8);
+    a = ((a & 0x0000FFFFU) << 16) | ((a & 0xFFFF0000U) >> 16);
+    return a >> (32 - len);
+}
+vector<c> fft(vector<c>& in, bool is_inv) {
+    int n = in.size();
+    int bitlen = __lg(n), z = is_inv ? -1 : 1;
+    vector<c> ret(n);
+    for (int i = 0; i < n; i++) ret[bit_rev(i, bitlen)] = in[i];
+    for (int s = 2, m = 1; s <= n; s <<= 1, m <<= 1) {
+        for (int i = 0; i < m; i++) {
+            c w = exp(c(0, i * z * PI / m));
+            for (int j = i, k = i + m; j < n; j += s, k += s) {
+                c u = ret[j], t = w * ret[k];
+                ret[j] = u + t, ret[k] = u - t;
             }
         }
-        for (int i = 0; is_inv && i < N; ++i)
-            out[i] /= N;
     }
-};
-int main () { // polynomial multiplication
-	FFT<double> F; int n = 4;
-	vector<complex<double>> a = {1, 2, 0, 0};
-	vector<complex<double>> b = {2, 3, 0, 0};
-	vector<complex<double>> a_fft(n), b_fft(n), ab_fft(n), ab(n);
-	F.fft(0, a, a_fft, 4), F.fft(0, b, b_fft, 4);
-	for (int i = 0; i < n; i++)
-		ab_fft[i] = a_fft[i] * b_fft[i];
-	F.fft(1, ab_fft, ab, n);
-	for (auto p : ab)
-		cout << int(p.real() + 1e-6) << " ";
-	return 0;
+    for (int i = 0; is_inv && i < n; i++) ret[i] /= n;
+    return ret;
+}
+#define T double  // 或者用 long long
+vector<T> poly_mul(vector<T>& arg1, vector<T>& arg2) { // 升冪
+    int n = ? // 二的指數且大於等於 arg1.size() + arg2.size() - 1
+    vector<c> a(arg1.begin(), arg1.end()), b(arg2.begin(), arg2.end()), abfft(n);
+    a.resize(n, 0.0), b.resize(n, 0.0);
+    auto afft = fft(a, 0), bfft = fft(b, 0);
+    for (int i = 0; i < n; i++) abfft[i] = afft[i] * bfft[i];
+    auto ab = fft(abfft, 1);
+    vector<T> ret;
+    for (auto val : ab) ret.push_back(val.real()); // 若是整數則四捨五入避免精度誤差
+    // 回傳的陣列會有很多後綴零，自己視情況去掉
+    return ret;
 }
